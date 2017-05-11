2881b059f32bbd64ba90484e2501e0ce8a1d9034
Abayomi Oloyinde <webpest@gmail.com>
2017-05-11 06:18:51 +0100

    Bootstrap setup

---
+++ app/core/App.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of App
+ *
+ * @author Abayomi
+ */
+class App {
+    public function __construct() {
+        echo 'OK!';
+    }
+}
